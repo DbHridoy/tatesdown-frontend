9cf60953aceadead64d6fd7e95d00a057dc237c0
DbHridoy <mdnuralam378@gmail.com>
2025-12-01 06:04:22 +0600

    (page): add new quote

--- src/UserComponents/Home/Pipeline.jsx
+++ src/UserComponents/Home/Pipeline.jsx
@@ -12,20 +12,21 @@ function Pipeline() {
   return (
     <div className="flex-1 bg-white shadow-md rounded-xl p-6">
       <h2 className="text-3xl font-semibold mb-4">Pipeline Overview</h2>
+
       <div className="bg-white rounded-xl p-2 w-full h-64">
         <ResponsiveContainer width="100%" height="100%">
           <BarChart
             layout="vertical"
-          data={pipelineData}
-          margin={{ top: 5, right: 20, left: 20, bottom: 5 }}
-        >
-          <XAxis type="number" />
-          <YAxis type="category" dataKey="stage" />
-          <Tooltip />
-          <Bar dataKey="value" fill="#3B82F6" barSize={20} />
-        </BarChart>
-      </ResponsiveContainer>
-    </div>
+            data={pipelineData}
+            margin={{ top: 5, right: 20, left: 20, bottom: 5 }}
+          >
+            <XAxis type="number" />
+            <YAxis type="category" dataKey="stage" />
+            <Tooltip />
+            <Bar dataKey="value" fill="#3B82F6" barSize={20} />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   );
 }
--- src/UserPages/Quotes/Quote.jsx
+++ src/UserPages/Quotes/Quote.jsx
@@ -1,11 +1,20 @@
 import Filters from "../../UserComponents/Clients/Filters";
 import ClientsTable from "../../UserComponents/Clients/ClientsTable";
+import { useNavigate } from "react-router-dom";
 function Quotes() {
+  const navigate=useNavigate()
   return (
     <div>
       <div className="flex justify-between flex-row ">
-        <h1>Quotes</h1>
-        <button className="bg-primarycolor text-white px-4 py-2 rounded">
+        <div>
+
+        <h1 className="text-2xl font-bold">Quotes</h1>
+        <p>Manage your quotes here</p>
+        </div>
+        <button 
+          className="bg-primarycolor text-white px-4 py-2 rounded"
+          onClick={() => navigate('/add-new-quote')}
+        >
           Add Quote
         </button>
       </div>
